9ab5233a0401ad37a1b00013de20eced09f74745
Caleb Crawley <crawleyhost@gmail.com>
2025-10-23 12:44:15 -0400

    Add Theme Toggle functionality

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,9 +3,14 @@ import React from "react";
 export default function About() {
 
     return (
-        <div className="min-h-screen bg-slate-50 w-full flex items-center justify-center">
-            <div className="flex flex-column items-start max-w-3xl gap-5">
-                <h1>About Page</h1>
+        <div className="">
+            <div className="items-start gap-5">
+                <h1>About This App</h1>
+                <p>
+                    Welcome to my React portfolio app. This app is built with Vite and Tailwind CSS.
+                    As you can see, there are multiple features available to check out, such as Password Authentication, Dark mode theme toggle and an interactive To Do List. <br/>
+                    This to do list pulls data from an API, a local file and your submitted data, combines them into a single list and displays them for you, with the option of completing, resetting and deleting each item.
+                </p>
             </div>
         </div>
     );
